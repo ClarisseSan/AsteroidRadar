8e09cc6bfb36b3e6ecf48d8b73966abc7afb44fe
unknown <sanchezclarisse@gmail.com>
2023-01-18 17:46:37 +0800

    change URL value

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
+    const val API_KEY = "9MsA3RstB0RSZTAeY8VEeITJ7GfG0Mpj3eGdmPLQ"
+    const val NASA_URL = "https://api.nasa.gov/planetary/apod?api_key=9MsA3RstB0RSZTAeY8VEeITJ7GfG0Mpj3eGdmPLQ"
 }
